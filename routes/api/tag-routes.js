0d3975bb21381cb822bbedd0e68f42a81313e7ee
bashratchy <bashrat03@gmail.com>
2023-09-13 19:19:58 -0400

    added routes for posting, updating and deleting tags

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -1,9 +1,9 @@
-const router = require('express').Router();
-const { Tag, Product, ProductTag } = require('../../models');
+const router = require("express").Router();
+const { Tag, Product, ProductTag } = require("../../models");
 
 // The `/api/tags` endpoint
 
-router.get('/', async (req, res) => {
+router.get("/", async (req, res) => {
   // find all tags
   // be sure to include its associated Product data
   try {
@@ -14,7 +14,7 @@ router.get('/', async (req, res) => {
   }
 });
 
-router.get('/:id', async (req, res) => {
+router.get("/:id", async (req, res) => {
   // find a single tag by its `id`
   // be sure to include its associated Product data
   const TagId = req.params.id;
@@ -27,17 +27,56 @@ router.get('/:id', async (req, res) => {
   }
 });
 
-
-router.post('/', (req, res) => {
+router.post("/", async (req, res) => {
   // create a new tag
+  const tagName = req.body.tag_name;
+
+  try {
+    const data = await Tag.create({ tag_name: tagName });
+    res.json(data);
+  } catch (err) {
+    res.status(400).json(err);
+  }
 });
 
-router.put('/:id', (req, res) => {
+router.put("/:id", async (req, res) => {
   // update a tag's name by its `id` value
+  try {
+    const TagId = req.params.id;
+    const { tag_name } = req.body; // To update tag name
+
+    // Find the tag by ID
+    const tag = await Tag.findByPk(TagId);
+
+    if (!tag) {
+      return res.status(404).json(err);
+    }
+
+    tag.tag_name = tag_name; // Updating the tag_name property
+
+    // Save the updated tag
+    await tag.save();
+
+    res.json({ message: "Tag updated successfully", tag });
+  } catch (err) {
+    res.status(400).json(err);
+  }
 });
 
-router.delete('/:id', (req, res) => {
+router.delete("/:id", async (req, res) => {
   // delete on tag by its `id` value
+  const tagId = req.params.id;
+
+  try {
+    await Tag.destroy({
+      where: {
+        id: tagId,
+      },
+    });
+    res.json({});
+  } catch (err) {
+    res.status(400).json(err);
+  }
 });
 
 module.exports = router;
